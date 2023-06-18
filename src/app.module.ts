e8ca110f91be0988aaf28c2b0eeb363468202213
AdiletMaksatuly <bakeryamaker1@gmail.com>
2023-06-19 04:47:05 +0600

    feat: add typeorm and postgresql driver for node.js, setup connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { FavouriteModule } from './favourite/favourite.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -16,6 +17,16 @@ import { FavouriteModule } from './favourite/favourite.module';
     ArtistModule,
     AlbumModule,
     FavouriteModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      synchronize: false,
+      autoLoadEntities: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
